5de020f9e8c9ac26080896cdeeb991cb3aa7a69e
Thomas Auinger <thomas@auinger.de>
2022-11-03 14:58:07 +0100

    removed everything from branch main

--- src/main/java/pizza/product/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pizza.product;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-
-}
