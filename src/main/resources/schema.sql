d59d357e74ee4ba4fca3e5bcdddcbbb1b56da636
Thomas Auinger <thomas@auinger.de>
2022-11-03 10:14:42 +0100

    added bit more SQL setup; formatted file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,29 @@
+DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
 
-CREATE TABLE products (
-    pk VARCHAR(10) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    price FLOAT NOT NULL,
+CREATE TABLE products
+(
+    pk    VARCHAR(10) NOT NULL,
+    name  VARCHAR(50) NOT NULL,
+    price FLOAT       NOT NULL,
     PRIMARY KEY (`pk`)
 );
 
-CREATE TABLE orders (
-    id LONG NOT NULL,
-    cst_id LONG NOT NULL,
+CREATE TABLE orders
+(
+    id          LONG  NOT NULL,
+    cst_id      LONG  NOT NULL,
     total_price FLOAT NOT NULL,
-    eta TIMESTAMP,
-    PRIMARY KEY (id)
+    eta         TIMESTAMP,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE customers (
-    -- TODO
+CREATE TABLE customers
+(
+    id LONG NOT NULL,
+    -- TODO other fields
+    PRIMARY KEY (`id`)
 );
 
 ALTER TABLE orders
