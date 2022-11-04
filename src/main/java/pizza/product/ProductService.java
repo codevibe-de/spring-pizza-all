4de9efd1e47b171bccc89c80bdf61924af834a97
Thomas Auinger <thomas@auinger.de>
2022-11-04 09:59:36 +0100

    removed getTotalPrice implementation

--- src/main/java/pizza/product/ProductService.java
+++ src/main/java/pizza/product/ProductService.java
@@ -32,14 +32,9 @@ public class ProductService {
     }
 
     public Double getTotalPrice(Map<String, Integer> productQuantities) {
-        // loop over each map entry (which is productId -> quantity) and map each entry to the product's price
-        // multiplied by desired quantity. Then sum all up and that is our total.
-        return productQuantities.entrySet().stream()
-                .mapToDouble(entry -> {
-                    Product product = getProduct(entry.getKey());
-                    return product.price * entry.getValue();
-                })
-                .sum();
+        double result = 0.0;
+        // TODO implement calculation
+        return result;
     }
 
     public Product createProduct(Product product) {
