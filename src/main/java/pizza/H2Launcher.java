7cc33d059a8558c96f7186e8e6bd709fdbbb809f
Thomas Auinger <thomas@auinger.de>
2022-11-06 18:48:24 +0100

    added check for already running H2 server

--- src/main/java/pizza/H2Launcher.java
+++ src/main/java/pizza/H2Launcher.java
@@ -1,9 +1,11 @@
 package pizza;
 
 import org.h2.tools.Server;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.sql.SQLException;
 
 // note that this class can be simplified to a @Bean declaration, as e.g. illustrated here:
@@ -11,14 +13,30 @@ import java.sql.SQLException;
 @Component
 public class H2Launcher implements Runnable {
 
+    private Server server;
+
     @PostConstruct
     @Override
     public void run() {
         System.out.println("Launching H2 TCP Server");
         try {
-            Server.createTcpServer("-tcp", "-ifNotExists", "-tcpPort", "9092").start();
+            server = Server.createTcpServer("-tcp", "-ifNotExists", "-tcpPort", "9092").start();
         } catch (SQLException e) {
-            throw new IllegalStateException("Failed to launch H2 TCP server", e);
+            var rootCause = NestedExceptionUtils.getRootCause(e);
+            if (rootCause != null && rootCause.getMessage().equals("Address already in use: bind")) {
+                System.out.println("Server seems to be running already (maybe in some other context?)");
+            }
+            else {
+                throw new IllegalStateException("Failed to launch H2 TCP server", e);
+            }
+        }
+    }
+
+    @PreDestroy
+    public void stop() {
+        if (this.server != null) {
+            System.out.println("Stopping H2 TCP Server");
+            this.server.stop();
         }
     }
 }
