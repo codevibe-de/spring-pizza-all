99c66ff0fd883d47dbb6be896a3af7ed45f6833e
Thomas Auinger <thomas@auinger.de>
2022-11-24 12:35:40 +0100

    code format

--- src/test/java/pizza/customer/CustomerServiceTest.java
+++ src/test/java/pizza/customer/CustomerServiceTest.java
@@ -1,6 +1,5 @@
 package pizza.customer;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
