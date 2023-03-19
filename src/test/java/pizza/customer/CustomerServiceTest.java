4c081aa33911ee53197fc17f56f6522555909e48
Thomas Auinger <thomas@auinger.de>
2023-03-19 21:34:20 +0100

    changed to @DataJpaTest, removed manual runner launch

--- src/test/java/pizza/customer/CustomerServiceTest.java
+++ src/test/java/pizza/customer/CustomerServiceTest.java
@@ -1,29 +1,18 @@
 package pizza.customer;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import pizza.SampleDataLoaderRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith({SpringExtension.class})
+@DataJpaTest
 @Import(CustomerTestContextConfiguration.class)
 public class CustomerServiceTest {
 
     @Autowired
     private CustomerService customerService;
-
-    @Autowired
-    private SampleDataLoaderRunner sampleDataLoaderRunner;
-
-    @BeforeEach
-    public void init() {
-        sampleDataLoaderRunner.run(null);
-    }
 
     @Test
     public void getCustomer() {
