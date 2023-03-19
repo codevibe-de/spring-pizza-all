d7406de4d55b8dd905f288ec2765d27b31ec2fa1
Thomas Auinger <thomas@auinger.de>
2023-03-19 20:51:56 +0100

    removed h2runner

--- src/test/java/pizza/customer/CustomerTestContextConfiguration.java
+++ src/test/java/pizza/customer/CustomerTestContextConfiguration.java
@@ -3,7 +3,6 @@ package pizza.customer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import pizza.H2ScriptRunner;
 import pizza.SampleDataLoader;
 import pizza.SampleDataLoaderRunner;
 
@@ -11,8 +10,7 @@ import pizza.SampleDataLoaderRunner;
 @ComponentScan({"pizza.customer", "pizza.product"})
 @Import({
         SampleDataLoaderRunner.class,
-        SampleDataLoader.SmallDataLoader.class,
-        H2ScriptRunner.class
+        SampleDataLoader.SmallDataLoader.class
 })
 public class CustomerTestContextConfiguration {
 }
