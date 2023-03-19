279b957a1c41cf2ba4424a2d6c1bb765b9bf70fb
Thomas Auinger <thomas@auinger.de>
2023-03-19 21:20:57 +0100

    refactored CustServTest

--- src/test/java/pizza/customer/CustomerServiceTest.java
+++ src/test/java/pizza/customer/CustomerServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pizza.SampleDataLoaderRunner;
@@ -13,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({SpringExtension.class})
 @Import(CustomerTestContextConfiguration.class)
-@AutoConfigureTestDatabase
 public class CustomerServiceTest {
 
     @Autowired
