bc64c14bfb182dbb919880ef79984aa76f35a294
Thomas Auinger <thomas@auinger.de>
2023-03-22 15:11:35 +0100

    sltn 120 done

---
+++ src/test/java/pizza/product/ProductRequestDataCsvConverterTest.java
@@ -0,0 +1,34 @@
+package pizza.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpInputMessage;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+class ProductRequestDataCsvConverterTest {
+
+    @Test
+    void readInternal() throws IOException {
+        // given
+        var csv = "productId,name,price\na,b,1";
+        var inputStream = new ByteArrayInputStream(csv.getBytes());
+        var mockedMessage = Mockito.mock(HttpInputMessage.class);
+        Mockito.when(mockedMessage.getBody()).thenReturn(inputStream);
+
+        // when
+        var dataList = new ProductRequestDataCsvConverter().readInternal(
+                null,
+                mockedMessage
+        );
+
+        // then
+        Assertions.assertThat(dataList).isNotNull();
+        Assertions.assertThat(dataList).hasSize(1);
+        Assertions.assertThat(dataList.get(0).getProductId()).isEqualTo("a");
+        Assertions.assertThat(dataList.get(0).getName()).isEqualTo("b");
+        Assertions.assertThat(dataList.get(0).getPrice()).isEqualTo(1);
+    }
+}
