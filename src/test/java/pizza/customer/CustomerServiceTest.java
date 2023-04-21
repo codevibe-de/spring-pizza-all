610961a37da42cd8b11d38009948efaf442977a9
Thomas Auinger <thomas@auinger.de>
2023-04-21 11:21:36 +0200

    040 sltn new for d)

--- src/test/java/pizza/customer/CustomerServiceTest.java
+++ src/test/java/pizza/customer/CustomerServiceTest.java
@@ -1,39 +1,47 @@
 package pizza.customer;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pizza.SampleDataLoader;
 import pizza.SampleDataLoaderRunner;
+import pizza.product.ProductService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({SpringExtension.class})
-@Import(CustomerTestContextConfiguration.class)
 public class CustomerServiceTest {
 
     @Autowired
-    private CustomerService customerService;
+    CustomerService customerService;
 
     @Autowired
-    private SampleDataLoaderRunner sampleDataLoaderRunner;
-
-    @BeforeEach
-    public void init() {
-        sampleDataLoaderRunner.run(null);
-    }
+    SampleDataLoaderRunner sampleDataLoaderRunner;
 
     @Test
-    public void getCustomer() {
+    void getCustomerByPhoneNumber() {
         // given
+        sampleDataLoaderRunner.run(null);
+        String phoneNumber = "+49 123 456789";
 
         // when
-        var customer = customerService.getCustomerByPhoneNumber("+49 123 456789");
+        Customer customer = customerService.getCustomerByPhoneNumber(phoneNumber);
 
         // then
         assertThat(customer).isNotNull();
-        assertThat(customer.getFullName()).isEqualTo("Enrico Pallazzo");
+        assertThat(customer.getPhoneNumber()).isEqualTo(phoneNumber);
+    }
+
+    @TestConfiguration
+    @ComponentScan("pizza.customer") // loads EVERY bean from package including the CustomerService
+    @Import({SampleDataLoaderRunner.class, SampleDataLoader.SmallDataLoader.class})
+    static class TestConfig {
+        @MockBean
+        ProductService productService;
     }
 }
