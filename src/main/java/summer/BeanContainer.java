0f9b12f5e6cc5d6bcf30b8405beb45fc52ebc6ea
Thomas Auinger <thomas@auinger.de>
2023-12-09 20:56:30 +0100

    bean cntr prep completed

--- src/main/java/summer/BeanContainer.java
+++ src/main/java/summer/BeanContainer.java
@@ -1,10 +1,10 @@
 package summer;
 
+import summer.exception.NoSuchBeanDefinitionException;
+import summer.exception.NoUniqueBeanDefinitionException;
+
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BeanContainer {
 
@@ -39,7 +39,49 @@ public class BeanContainer {
     // --- internal helper methods ---
 
     private Constructor<?> findConstructor(Class<?> beanClass) {
-        return beanClass.getConstructors()[0];
+        var constructors = beanClass.getDeclaredConstructors();
+        if (constructors.length != 1) {
+            throw new IllegalStateException("Class " + beanClass.getName() + " does not provide a single constructor");
+        }
+        return constructors[0];
+    }
+
+    private Class<?>[] findConstructorParameterTypes(Class<?> type) {
+        Constructor<?> constructor = findConstructor(type);
+        return constructor.getParameterTypes();
+    }
+
+    Map<String,Set<String>> createBeanDependencyMap(Collection<BeanDefinition> defs) {
+        var map = new HashMap<String, Set<String>>();
+        for (var def : defs) {
+            Class<?>[] constructorParamTypes = findConstructorParameterTypes(def.getType());
+            String[] constructorParamBeanNames = resolveBeanNames(constructorParamTypes, defs);
+            map.put(def.getName(), new HashSet<>(Arrays.asList(constructorParamBeanNames)));
+        }
+        return map;
+    }
+
+    String[] resolveBeanNames(Class<?>[] types, Collection<BeanDefinition> defs) {
+        var names = new String[types.length];
+        for (int n=0; n<types.length; n++) {
+            names[n] = resolveBeanName(types[n],  defs);
+        }
+        return names;
+    }
+
+
+    String resolveBeanName(Class<?> type, Collection<BeanDefinition> defs) {
+        // we want to find all bean-definitions (and use their name) that satisfy the given
+        // type, i.e. that implement that type
+        var candidateNames = defs.stream()
+                .filter(def -> def.satisfies(type))
+                .map(BeanDefinition::getName)
+                .toList();
+        return switch (candidateNames.size()) {
+            case 0 -> throw new NoSuchBeanDefinitionException(type);
+            case 1 -> candidateNames.get(0);
+            default -> throw new NoUniqueBeanDefinitionException(type);
+        };
     }
 
 }
--- src/main/java/summer/BeanDefinition.java
+++ src/main/java/summer/BeanDefinition.java
@@ -17,4 +17,19 @@ public final class BeanDefinition {
     public Class<?> getType() {
         return type;
     }
+
+    /**
+     * Returns {@code true}, if the bean created by this {@code BeanDefinition} could be assigned to
+     * the given type, i.e. satisfies the given type requirement.
+     * <p>Example: You have a bean definition for type {@code MyRunnable}. That class implements
+     * {@code Runnable}. The bean
+     * created from this definition could be assigned to both a variable of type {@code MyRunnable}
+     * and {@code Runnable} (and even {@code Object}). Hence, this bean definition would satisfy the
+     * required types {@code MyRunnable.class}, {@code Runnable.class} and {@code Object.class}.
+     */
+    public boolean satisfies(Class<?> requiredType) {
+        // "isAssignableFrom" is hard to read/understand. It basically means if it is okay to write:
+        // var <requiredType> = new <this.type>()
+        return requiredType.isAssignableFrom(type);
+    }
 }
