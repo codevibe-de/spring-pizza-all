12e9f341250ad49b59aa767e718671bed4fd7c8b
Thomas Auinger <thomas@auinger.de>
2023-12-10 08:07:29 +0100

    extnd bean exceptions

--- src/main/java/summer/exception/BeansException.java
+++ src/main/java/summer/exception/BeansException.java
@@ -1,7 +1,13 @@
 package summer.exception;
 
 public class BeansException extends RuntimeException {
+
     public BeansException(String msg) {
         super(msg);
     }
+
+    public BeansException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
 }
