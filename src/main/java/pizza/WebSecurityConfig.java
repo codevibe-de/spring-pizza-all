54aa6d1fbbe77d39457f41487b17fa86e2b9b175
Thomas Auinger <thomas@auinger.de>
2023-12-15 19:30:07 +0100

    180-sltn fixes including new requests

--- src/main/java/pizza/WebSecurityConfig.java
+++ src/main/java/pizza/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package pizza;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,32 +17,31 @@ import pizza.order.OrderRestController;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
-@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.authorizeRequests()
-                .antMatchers(GET, WhoAmIController.ME_ENDPOINT).permitAll()
-                .antMatchers(GET, OrderRestController.GREETING_ENDPOINT).permitAll()
-                .antMatchers(POST, OrderRestController.PLACE_ORDER_ENDPOINT).hasRole("CUSTOMER")
-                .antMatchers("/**").hasRole("ADMIN")
-            .and()
-                .httpBasic()
-            .and()
-                .csrf().disable(); // only for testing
-        // @formatter:on
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(GET, WhoAmIController.ME_ENDPOINT).permitAll()
+                        .requestMatchers(GET, OrderRestController.GREETING_ENDPOINT).permitAll()
+                        .requestMatchers(POST, OrderRestController.PLACE_ORDER_ENDPOINT).hasRole("CUSTOMER")
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable); // DEV ONLY
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public UserDetailsService userDetailsService(CustomerService customerService, PasswordEncoder passwordEncoder) {
         return new MyUserDetailsService(customerService, passwordEncoder);
-    }
-
-    @Bean
-    PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
