18d663f65e6a5bf995b326f0ba413f5fc4ac776c
Thomas Auinger <thomas@auinger.de>
2024-01-10 12:00:18 +0100

    011 sltn refactored

--- src/main/java/pizza/PizzaApp.java
+++ src/main/java/pizza/PizzaApp.java
@@ -1,12 +1,10 @@
 package pizza;
 
-import org.h2.jdbcx.JdbcDataSource;
 import pizza.customer.CustomerService;
 import pizza.order.OrderService;
-import pizza.product.JdbcProductRepository;
+import pizza.product.HashMapProductRepository;
 import pizza.product.ProductService;
-
-import javax.sql.DataSource;
+import summer.BeanContainer;
 
 import static java.util.Map.entry;
 import static java.util.Map.ofEntries;
@@ -15,12 +13,19 @@ public class PizzaApp {
 
     public static void main(String[] args) {
         // Instantiate beans ---
-        H2TcpServer h2TcpServer = startDatabase();
-        DataSource dataSource = createDataSource();
-        ProductService productService = new ProductService(new JdbcProductRepository(dataSource));
-        CustomerService customerService = new CustomerService();
-        OrderService orderService = new OrderService(customerService, productService);
-        new DataLoader.Sample(productService, customerService).run();
+        BeanContainer beanContainer = new BeanContainer();
+        beanContainer.defineBean("prdRepo", HashMapProductRepository.class);
+        beanContainer.defineBean("prdService", ProductService.class);
+        beanContainer.defineBean("cstmService", CustomerService.class);
+        beanContainer.defineBean("orderService", OrderService.class);
+        beanContainer.defineBean("sample", DataLoader.Sample.class);
+        beanContainer.refresh();
+
+        // query and use beans
+        beanContainer.getBean(DataLoader.class).run();
+        ProductService productService = beanContainer.getBean(ProductService.class);
+        CustomerService customerService = beanContainer.getBean(CustomerService.class);
+        OrderService orderService = beanContainer.getBean(OrderService.class);
 
         // Get a product ---
         var product = productService.getProduct("P-10");
@@ -39,32 +44,6 @@ public class PizzaApp {
                 )
         );
         System.out.println(order);
-
-        // Stop database to make app terminate ---
-        stopDatabase(h2TcpServer);
     }
 
-    static H2TcpServer startDatabase() {
-        H2TcpServer h2TcpServer = new H2TcpServer();
-        h2TcpServer.start();
-        return h2TcpServer;
-    }
-
-    static void stopDatabase(H2TcpServer h2TcpServer) {
-        if (h2TcpServer != null) {
-            h2TcpServer.stop();
-        }
-    }
-
-    static DataSource createDataSource() {
-        // use a H2 DataSource implementation
-        var dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:tcp://localhost:9092/~/training.spring.pizza");
-
-        // run a script to set up the database schema (=tables)
-        new SchemaScriptRunner(dataSource).run();
-
-        // return the data source for others to work with
-        return dataSource;
-    }
 }
