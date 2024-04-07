372932504eb1c6400fd2b1e187dab8d21b7521e1
Thomas Auinger <thomas@auinger.de>
2024-04-07 18:52:22 +0200

    removed summer

--- src/main/java/summer/exception/BeanInitializationException.java
+++
@@ -1,7 +0,0 @@
-package summer.exception;
-
-public class BeanInitializationException extends BeansException {
-    public BeanInitializationException(String beanName, Throwable cause) {
-        super("Failed to initialize bean '" + beanName + "'", cause);
-    }
-}
