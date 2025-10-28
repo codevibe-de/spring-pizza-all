f71de9909ab955c35ad81580806d99e57adabe69
Thomas Auinger <thomas@auinger.de>
2025-10-28 11:16:22 +0100

    moved mockito bean to class level, since it works differently than @MockBean

--- src/test/java/pizza/customer/CustomerServiceTest.java
+++ src/test/java/pizza/customer/CustomerServiceTest.java
@@ -16,7 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 // Note: Context is configured in inner @TestConfiguration class below
 @ExtendWith({SpringExtension.class})
+@Import(CustomerServiceTest.TestConfig.class)
 public class CustomerServiceTest {
+
+    @MockitoBean
+    ProductService productService;
 
     @Autowired
     CustomerService customerService;
@@ -46,7 +50,5 @@ public class CustomerServiceTest {
     @ComponentScan("pizza.customer") // loads EVERY bean from package including the CustomerService
     @Import({DataLoader.Sample.class})
     static class TestConfig {
-        @MockitoBean // we don't care what the ProductService does, we just need that bean in the context for data loading
-        ProductService productService;
     }
 }
