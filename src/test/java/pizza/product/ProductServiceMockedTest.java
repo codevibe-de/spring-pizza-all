69db64016bb8ec65e792b163272b6686cfd92021
Thomas Auinger <thomas@auinger.de>
2025-10-28 09:10:44 +0100

    040-exrc @MockitoBean

--- src/test/java/pizza/product/ProductServiceMockedTest.java
+++ src/test/java/pizza/product/ProductServiceMockedTest.java
@@ -10,7 +10,7 @@ class ProductServiceMockedTest {
     @Autowired
     ProductService productService;
 
-    // @MockBean
+    // @MockitoBean
     // ProductRepository productRepository;
 
     /**
