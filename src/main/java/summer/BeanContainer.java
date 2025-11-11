866adeb329203842794e0d066f2e2838c932a96f
Thomas Auinger <thomas@auinger.de>
2025-11-11 11:56:33 +0100

    removed duplicate

--- src/main/java/summer/BeanContainer.java
+++ src/main/java/summer/BeanContainer.java
@@ -82,11 +82,6 @@ public class BeanContainer {
                 .findAny().orElseThrow(() -> new NoSuchBeanDefinitionException("beanName"));
     }
 
-    private BeanDefinition getBeanDefinition(String beanName) {
-        return beanDefinitions.stream().filter(def -> def.getName().equals(beanName))
-                .findAny().orElseThrow(() -> new NoSuchBeanDefinitionException("beanName"));
-    }
-
 
     /**
      * Returns the bean that can be assigned to the given type.
