4dbc707993b1a69a5315671c9d5bf6de1fc1b22a
Thomas Auinger <thomas@auinger.de>
2025-11-12 14:58:17 +0100

    050 sltn added projection with test

--- src/main/java/pizza/customer/CustomerRepository.java
+++ src/main/java/pizza/customer/CustomerRepository.java
@@ -2,10 +2,13 @@ package pizza.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByPhoneNumber(String pn);
+
+    List<CustomerPhoneNumberAndPostalCode> findAllProjectedBy();
 
 }
---
+++ src/test/java/pizza/customer/CustomerRepositoryTest.java
@@ -0,0 +1,28 @@
+package pizza.customer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void findAllCustomerPhoneNumberAndZip() {
+        // when
+        List<CustomerPhoneNumberAndPostalCode> projections = customerRepository.findAllProjectedBy();
+
+        // then
+        Assertions.assertThat(projections).hasSize(2);
+        Assertions.assertThat(projections).anySatisfy(c -> {
+            Assertions.assertThat(c.getPhoneNumber()).isEqualTo("+49 123 456789");
+            Assertions.assertThat(c.getAddress().getPostalCode()).isEqualTo("40302");
+        });
+    }
+}
