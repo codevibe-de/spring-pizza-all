299b7cd2d8dd890333bd3ede0fe06bb3027f5d9c
Thomas Auinger <thomas@auinger.de>
2026-07-02 21:38:22 +0200

    045 sltn for a)

--- src/main/java/pizza/PersistenceConfig.java
+++ src/main/java/pizza/PersistenceConfig.java
@@ -1,20 +1,24 @@
 package pizza;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ConditionalOnProperty(name = "persistence-config.enabled")
 public class PersistenceConfig {
 
-//    @Bean
-//    public DataSource dataSource() {
-//        var ds = new DriverManagerDataSource();
-//        ds.setDriverClassName("org.h2.Driver");
-//        ds.setUrl("jdbc:h2:tcp://localhost:9092/./pizzadb");
-//        ds.setUsername("sa");
-//        ds.setPassword("");
-//        return ds;
-//    }
+    @Bean
+    public DataSource dataSource() {
+        var ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:tcp://localhost:9092/./pizzadb");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
 
 }
